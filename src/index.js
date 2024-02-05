1372dd7cf600ef13266f54bd320728fae8200597
RaulPabianCruz <ito.pabian@gmail.com>
2024-02-05 15:29:40 -0800

    Add a search function for the weather forecast
    Adds a search bar which allows users to submit a fetch request to the
    weather api for forecast data. The request is processesed with helper
    functions to return only relevant data.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+async function getWeather(cityLocation) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=72b96543d1d549ab8d1205722242901&q=${cityLocation}&days=3`,
+      { mode: 'cors' },
+    );
+    const forecastData = await response.json();
+    return forecastData;
+  } catch (error) {
+    return error;
+  }
+}
 
+function processResponseJSON(response) {
+  const forecastArray = response.forecast.forecastday;
+  const locationObject = response.location;
+  return [Promise.resolve(forecastArray), Promise.resolve(locationObject)];
+}
+
+function processForecastDays(forecastArray) {
+  const newArray = forecastArray.map((forecastday) => {
+    const tempCelsius = forecastday.day.avgtemp_c;
+    const tempFarenheit = forecastday.day.avgtemp_f;
+    const conditionText = forecastday.day.condition.text;
+    const conditionIcon = forecastday.day.condition.icon;
+    return { tempCelsius, tempFarenheit, conditionText, conditionIcon };
+  });
+  return newArray;
+}
+
+function requestWeather(event) {
+  const regionName = event.target.value;
+  const weatherPromise = getWeather(regionName);
+  weatherPromise
+    .then(
+      (response) => Promise.all(processResponseJSON(response)),
+      (error) => console.log(error),
+    )
+    .then(
+      (forecastData) => {
+        const weatherArray = processForecastDays(forecastData[0]);
+        return {
+          country: forecastData[1].country,
+          city: forecastData[1].name,
+          state: forecastData[1].region,
+          weatherDataArray: weatherArray,
+        };
+      },
+      (error) => console.log(error),
+    )
+    .then((dataObject) => console.log(dataObject));
+}
+
+const searchBar = document.querySelector('.search-bar');
+searchBar.addEventListener('search', requestWeather);
